cb9f34691e4b571ae0857c1efc24e489a658ec49
igorct1 <igorct77@gmail.com>
2023-11-16 18:31:46 -0300

    fix: problem with tailwind classes

--- src/components/HeroSection/AddToCart/index.tsx
+++ src/components/HeroSection/AddToCart/index.tsx
@@ -57,9 +57,9 @@ export function AddToCart() {
         </button>
 
         <Toast.Root
-          className=" data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=end]:animate-swipeOut grid grid-cols-[auto_max-content] items-center gap-x-[15px] rounded-md bg-orange p-[15px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] [grid-template-areas:_'title_action'_'description_action'] data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:transition-[transform_200ms_ease-out]"
           open={open}
           onOpenChange={setOpen}
+          className=" grid grid-cols-[auto_max-content] items-center gap-x-[15px] rounded-md bg-orange p-[15px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] [grid-template-areas:_'title_action'_'description_action'] data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[state=closed]:animate-hide data-[state=open]:animate-slideIn data-[swipe=end]:animate-swipeOut data-[swipe=cancel]:transition-[transform_200ms_ease-out]"
         >
           <Toast.Title className="mb-[5px] text-[15px] font-semibold text-zinc-50 [grid-area:_title]">
             Item adicionado ao carrinho
